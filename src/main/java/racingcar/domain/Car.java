68b0d28173af31a35336346f399d4ada80a96cf8
COBI-98 <tkdgus968@naver.com>
2025-10-27 20:50:57 +0900

    feat(car): 자동차에 위치를 도입하여 책임 위임
    - 자동차가 생성될 때 시작 위치를 0으로 갖도록 역할 부여

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,12 +3,18 @@ package racingcar.domain;
 public class Car {
 
     private CarName carName;
+    private Distance distance;
 
     public Car(final String name) {
         this.carName = new CarName(name);
+        distance = new Distance();
     }
 
     public CarName getCarName() {
         return carName;
+    }
+
+    public Distance getDistance() {
+        return distance;
     }
 }
