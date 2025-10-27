717cacbdbc9ccb6da41101192126f84e9633fb9a
COBI-98 <tkdgus968@naver.com>
2025-10-27 21:10:21 +0900

    feat(outputView): 실행결과를 담을 출력 뷰 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String EXECUTION_RESULT = "실행 결과";
+    private static final String NEW_LINE = "\n";
+
+    public void printExecutionResult(){
+        System.out.println(NEW_LINE);
+        System.out.println(EXECUTION_RESULT);
+    }
+}
