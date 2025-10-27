2633bdb3f68ce638b2d7f3da06722933f4b9c1b4
COBI-98 <tkdgus968@naver.com>
2025-10-27 21:15:37 +0900

    feat(controller): 경주 자동차 생성 및 시도횟수 메서드 생성

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -2,19 +2,30 @@ package racingcar.controller;
 
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.TryCount;
 import racingcar.util.CarInputParser;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
 
     private final InputView inputView;
+    private final OutputView outputView;
 
     public RacingController() {
         inputView = new InputView();
+        outputView = new OutputView();
     }
 
     public void start() {
-        List<Car> carList = CarInputParser.splitCarNames(inputView.inputCarNames());
-
+        Cars racingCars = createRacingCars();
+        TryCount tryCount = new TryCount(inputView.inputAttempts());
     }
+
+    private Cars createRacingCars() {
+        List<Car> carList = CarInputParser.splitCarNames(inputView.inputCarNames());
+        return new Cars(carList);
+    }
+
 }
