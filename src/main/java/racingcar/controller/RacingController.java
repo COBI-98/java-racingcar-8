617170fd77fe910a4ebecdf36437724ff12a78df
COBI-98 <tkdgus968@naver.com>
2025-10-27 22:21:47 +0900

    feat(controller): 무작위 값 추가 및 자동차 전진 출력 루프 추가
    - ㄹ

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -5,6 +5,8 @@ import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.TryCount;
 import racingcar.util.CarInputParser;
+import racingcar.util.CarRandomMoveGenerator;
+import racingcar.util.RandomUtils;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -12,6 +14,7 @@ public class RacingController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final RandomUtils racingCarRandomMoveUtils = new CarRandomMoveGenerator();
 
     public RacingController() {
         inputView = new InputView();
@@ -21,11 +24,22 @@ public class RacingController {
     public void start() {
         Cars racingCars = createRacingCars();
         TryCount tryCount = new TryCount(inputView.inputAttempts());
+
+        racing(racingCars, tryCount);
     }
 
     private Cars createRacingCars() {
         List<Car> carList = CarInputParser.splitCarNames(inputView.inputCarNames());
         return new Cars(carList);
+    }
+
+    private void racing(Cars racingCars, TryCount tryCount) {
+        outputView.printExecutionResult();
+
+        for (int i = 0; i < tryCount.getTryCount(); i++) {
+            racingCars.moveAll(racingCarRandomMoveUtils);
+            outputView.printRoundByExecutionResults(racingCars);
+        }
     }
 
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,27 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
 public class OutputView {
 
     private static final String EXECUTION_RESULT = "실행 결과";
     private static final String NEW_LINE = "\n";
+    private static final String COLON_SIGN = " : ";
+    private static final String MOVE_SIGN = "-";
+    private static final String COMMA_SIGN = ", ";
 
     public void printExecutionResult(){
-        System.out.println(NEW_LINE);
+        System.out.print(NEW_LINE);
         System.out.println(EXECUTION_RESULT);
+    }
+
+    public void printRoundByExecutionResults(final Cars cars) {
+        StringBuilder resultByRound = new StringBuilder();
+        for (Car car : cars.getCars()) {
+            resultByRound.append(car.getCarName())
+                    .append(COLON_SIGN)
+                    .append(MOVE_SIGN.repeat(car.getDistance().toInt()))
+                    .append(NEW_LINE);
+        }
+        System.out.println(resultByRound);
     }
 }
